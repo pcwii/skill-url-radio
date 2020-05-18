f02bff3ad1811fb1650ef34bd371fce50f3a41d0
philc <philc@arcx.com>
2020-05-18 12:45:24 -0400

    First Commit with test url's
    Change-Id: If8db27cfb4fe65f7ebd6456355171cf197f0e51e

--- __init__.py
+++ __init__.py
@@ -27,7 +27,7 @@ class URLRadio(CommonPlaySkill):
         file_name = 'url_list.json'
         skill_path = "/opt/mycroft/skills/skill-url-radio.pcwii"
         with open(join(skill_path, file_name)) as f:
-            self.channel_list = json.loads(f)
+            self.channel_list = json.load(f)
         self.log.info(str(self.channel_list))
 
     def initialize(self):
