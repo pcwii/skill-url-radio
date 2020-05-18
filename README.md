bfed916b65d5930f0bedf0884d2913c1cdc9d55d
philc <philc@arcx.com>
2020-05-17 21:25:04 -0400

    First Commit with test url's
    Change-Id: Ic11e0df012f56ca6f6939be2c66c3198be8d407d

--- README.md
+++ README.md
@@ -1,21 +1,4 @@
-# Dutch Radio skill
+# URL Radio skill
 
-This skill streams radio from urls.
-Change the csv to add or change channels in to your own language.
-
-Add or change urls with streams you like from http://www.hendrikjansen.nl/henk/streaming.html
-
-It is based upon https://github.com/forslund/mycroft-media-skills/tree/master/swedishradio
-
-## Current state
-
-Working features:
- - turn on 3fm
- - stream radio 1
- - play radio arrow classic rock
-
-Known issues:
- - none, so far
-
-TODO:
- - Add volume adjustment when using wakeword.
+This skill will stream online radio stations from urls provided in the url_list.json.
+Update the json file to add more channels.
--- __init__.py
+++ __init__.py
@@ -1,94 +1,82 @@
 ﻿import sys
 from os.path import dirname, abspath
-
-from adapt.intent import IntentBuilder
-from mycroft.skills.core import MycroftSkill
-from mycroft.util.log import getLogger
 import time
 import subprocess
+import json
+
+
+from mycroft.skills.common_play_skill import CommonPlaySkill, CPSMatchLevel
+from mycroft.util.parse import match_one
+
+#import re
+#from adapt.intent import IntentBuilder
+#from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
 
 from mycroft.util.log import getLogger
 
 sys.path.append(abspath(dirname(__file__)))
-dr = __import__('dr')
+url = __import__('url')
 
 logger = getLogger(__name__)
 
-__author__ = 'tjoen'
+__author__ = 'pcwii20200517'
 
 
-class DutchRadio(MycroftSkill):
+class URLRadio(CommonPlaySkill):
     def __init__(self):
-        super(DutchRadio, self).__init__('Dutch Radio')
-        self.dr = dr.DutchRadio()
+        super(URLRadio, self).__init__('URL Radio')
+        self.url = url.URLRadio()
         self.process = None
+        self.regexes = {}
+        with open("url_list.json", "r") as channel_file:
+            self.channel_list = json.load(channel_file)
 
     def initialize(self):
-        logger.info('initializing DutchRadio')
+        logger.info('initializing URLRadio')
         self.load_data_files(dirname(__file__))
-        super(DutchRadio, self).initialize()
+        super(URLRadio, self).initialize()
+#        for c in self.url.channels.keys():
+#            self.register_vocabulary(c, 'ChannelKeyword')
+#        intent = IntentBuilder('PlayChannelIntent' + self.name)\
+#            .require('PlayKeyword')\
+#            .require('ChannelKeyword')\
+#            .build()
+#        self.register_intent(intent, self.handle_play_channel)
+#        intent = IntentBuilder('PlayFromIntent' + self.name)\
+#            .require('PlayKeyword')\
+#            .require('ChannelKeyword')\
+#            .require('NameKeyword')\
+#            .build()
+#        self.register_intent(intent, self.handle_play_channel)
 
-        for c in self.dr.channels.keys():
-            self.register_vocabulary(c, 'ChannelKeyword')
-        intent = IntentBuilder('PlayChannelIntent' + self.name)\
-            .require('PlayKeyword')\
-            .require('ChannelKeyword')\
-            .build()
-        self.register_intent(intent, self.handle_play_channel)
-        intent = IntentBuilder('PlayFromIntent' + self.name)\
-            .require('PlayKeyword')\
-            .require('ChannelKeyword')\
-            .require('NameKeyword')\
-            .build()
-        self.register_intent(intent, self.handle_play_channel)
 
-    def before_play(self):
+    def CPS_match_query_phrase(self, phrase):
         """
-           Stop currently playing media before starting the new. This method
-           should always be called before the skill starts playback.
+            The method is invoked by the PlayBackControlSkill.
         """
-        logger.info('Stopping currently playing media if any')
-        #if self.process:
-        #    self.stop()
-
-    def play(self):
-        self.before_play()
-        self.speak_dialog('listening_to', {'channel': self.channel})
-        time.sleep(2)
-        stream_url = self.dr.channels[self.channel].stream_url
-        self.process = subprocess.Popen(['mpg123', stream_url])
-
-    def get_available(self, channel_name):
-        logger.info(channel_name)
-        if channel_name in self.dr:
-            logger.info('Registring play intention...')
-            return channel_name
+        self.log.info('URLRadio received the following phrase: ' + phrase)
+        match, confidence = match_one(phrase, self.channel_list)
+        # If the confidence is high enough return a match
+        if confidence > 0.5:
+            return match, CPSMatchLevel.TITLE, {"track": match}
+        # Otherwise return None
         else:
             return None
 
-    def prepare(self, channel_name):
-        if self.process:
-            self.stop()
-        self.channel = channel_name
 
-    def handle_play_channel(self, message):
-        logger.debug( message.data )
-        c = message.data.get('ChannelKeyword')
-        self.prepare(c)
-        self.play()
-
-    def stop(self, message=None):
-        logger.info('Handling stop request')
-        if self.process:
-            self.process.terminate()
-            self.process.wait()
-            self.process = None
-            self.channel = None
-
-    def handle_currently_playing(self, message):
-        if self.channel is not None:
-            self.speak_dialog('currently_playing', {'channel': self.channel})
+    def CPS_start(self, phrase, data):
+        """ Starts playback.
+            Called by the playback control skill to start playback if the
+            skill is selected (has the best match level)
+        """
+        self.log.info('CPKodi Skill received the following phrase and Data: ' + phrase + ' ' + data['track'])
+#        self.speak_dialog('now.playing', data={"channel": play_request[0], "category": play_request[1]},
+#                          expect_response=False)
+        url = data['track']
+#        self.audioservice.play(url)  #
+        pass
 
 
 def create_skill():
-    return DutchRadio()
+    return URLRadio()
