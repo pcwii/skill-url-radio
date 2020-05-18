78f1ea4991eb9bedb002e0ce5d816c348452de8c
philc <philc@arcx.com>
2020-05-18 12:02:55 -0400

    First Commit with test url's
    Change-Id: I384cf7557783157b2fcc36450884b05db0f5ead8

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 ﻿import sys
 from os.path import dirname, abspath, join
+
+
 import time
 import subprocess
 import json
@@ -25,7 +27,7 @@ class URLRadio(CommonPlaySkill):
         file_name = 'url_list.json'
         skill_path = "/opt/mycroft/skills/skill-url-radio.pcwii"
         with open(join(skill_path, file_name)) as f:
-            self.channel_list = json.load(f)
+            self.channel_list = json.loads(f)
         self.log.info(str(self.channel_list))
 
     def initialize(self):
@@ -52,10 +54,15 @@ class URLRadio(CommonPlaySkill):
             The method is invoked by the PlayBackControlSkill.
         """
         self.log.info('URLRadio received the following phrase: ' + phrase)
-        #match, confidence = match_one(phrase, self.channel_list)
-        # If the confidence is high enough return a match
-        for name, url in self.channel_list["stations"]:  # loop through every interval
-            self.log.info("URLRadio returned: " + str(name), str(url))
+        for each_channel in self.channel_list["stations"]:  # loop through every interval
+            if phrase in each_channel["name"]:
+                print("Station Found, Exiting!")
+                data = each_channel
+                return phrase, CPSMatchLevel.TITLE, data
+                break
+            else:
+                print("Station Not Found, Searching!")
+
         #if confidence > 0.5:
             #return match, CPSMatchLevel.TITLE, {"track": match}
         # Otherwise return None
@@ -68,7 +75,7 @@ class URLRadio(CommonPlaySkill):
             Called by the playback control skill to start playback if the
             skill is selected (has the best match level)
         """
-        self.log.info('URLRadio Skill received the following phrase and Data: ' + phrase + ' ' + data['track'])
+        self.log.info('URLRadio Skill received the following phrase and Data: ' + phrase + ' ' + data)
 #        self.speak_dialog('now.playing', data={"channel": play_request[0], "category": play_request[1]},
 #                          expect_response=False)
         url = data['track']
